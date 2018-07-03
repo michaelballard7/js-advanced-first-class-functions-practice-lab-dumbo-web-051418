dc0116920dc2f4c5763af7ebb0871abec266c226
Michael Ballard <ballard.michael9@gmail.com>
2018-07-03 13:12:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,19 @@ const driversByRevenue = function (drivers) {
     return driverOne.revenue - driverTwo.revenue;
   });
 };
+
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
